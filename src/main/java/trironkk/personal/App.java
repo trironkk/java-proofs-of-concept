5f4426656b76a2bd8df246485220ffcf55425bc0
trironkk <trironk@gmail.com>
2016-04-11 06:22:02 +0000

    Altered main method to invoke a code path marked with an aspect annotation.

--- src/main/java/trironkk/personal/App.java
+++ src/main/java/trironkk/personal/App.java
@@ -4,10 +4,18 @@ package trironkk.personal;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        new App().appExecute(42, "Hello world!", 3.14);
+    }
+
+    @ExampleAnnotation
+    private void appExecute(
+            final int arg1,
+            final String arg2,
+            final double arg3) {
+        System.out.println("arg1: " + arg1);
+        System.out.println("arg2: " + arg2);
+        System.out.println("arg3: " + arg3);
     }
 }
